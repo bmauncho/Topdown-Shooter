56a96dc4ed6e71b80c8cdd4da03cebf78f83bd60
bmauncho <84400423+bmauncho@users.noreply.github.com>
2022-12-31 14:37:27 +0300

    Movement_Fix

--- Top_Down Shooter/Assets/Scripts/CharaterMovement.cs
+++ Top_Down Shooter/Assets/Scripts/CharaterMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CharaterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterController controller;
+    Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
